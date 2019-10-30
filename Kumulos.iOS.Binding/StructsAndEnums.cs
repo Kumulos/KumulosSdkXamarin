5db1e8505fdba19039ca1d910112c84e6775d84a
Andrew Lindsay <a.lindsay@kumulos.com>
2019-10-30 16:02:43 +0000

    Update bindings / static library.

--- Kumulos.iOS.Binding/StructsAndEnums.cs
+++ Kumulos.iOS.Binding/StructsAndEnums.cs
@@ -2,37 +2,37 @@ using ObjCRuntime;
 
 namespace Com.Kumulos.iOS
 {
-    [Native]
-    public enum KSTargetType : long
-    {
-        NotOverridden,
-        Debug,
-        Release
-    }
+	[Native]
+	public enum KSTargetType : long
+	{
+		NotOverridden,
+		Debug,
+		Release
+	}
 
-    [Native]
-    public enum KSInAppConsentStrategy : long
-    {
-        NotEnabled,
-        AutoEnroll,
-        ExplicitByUser
-    }
+	[Native]
+	public enum KSInAppConsentStrategy : long
+	{
+		NotEnabled,
+		AutoEnroll,
+		ExplicitByUser
+	}
 
-    [Native]
-    public enum KSErrorCode : long
-    {
-        NetworkError,
-        RpcError,
-        UnknownError,
-        ValidationError,
-        HttpBadStatus
-    }
+	[Native]
+	public enum KSErrorCode : long
+	{
+		NetworkError,
+		RpcError,
+		UnknownError,
+		ValidationError,
+		HttpBadStatus
+	}
 
-    [Native]
-    public enum KSInAppMessagePresentationResult : long
-    {
-        Presented,
-        Expired,
-        Failed
-    }
+	[Native]
+	public enum KSInAppMessagePresentationResult : long
+	{
+		Presented,
+		Expired,
+		Failed
+	}
 }
