0217a43a95ab835ea6245f2b62ec91aaab64bc37
Andrew Lindsay <a.lindsay@kumulos.com>
2020-01-23 10:36:08 +0000

    Bump nuget build, add a middleware push action handler to abstract the java object

---
+++ Com.Kumulos.Android/PushActionHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Android.Content;
+using Com.Kumulos.Android;
+
+namespace Com.Kumulos
+{
+    public class PushActionHandler : Java.Lang.Object, IPushActionHandlerInterface
+    {
+        void IPushActionHandlerInterface.Handle(Context context, PushMessage pushMessage, string actionId)
+        {
+            throw new NotImplementedException("You must create your own implementation subclass to handle button actions");
+        }
+    }
+}
