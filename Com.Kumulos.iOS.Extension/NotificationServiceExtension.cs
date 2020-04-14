88b62f7dd81feca5b3fbc3a350f08cfde8f4d776
Andrew Lindsay <a.lindsay@kumulos.com>
2020-04-14 14:37:34 +0100

    Move / rename extension, missed a .

--- Com.Kumulos.iOS.Extension/NotificationServiceExtension.cs
+++ Com.Kumulos.iOS.Extension/NotificationServiceExtension.cs
@@ -6,10 +6,25 @@ namespace Com.Kumulos
 {
     public class NotificationServiceExtension
     {
+        static readonly Lazy<NotificationServiceExtension> extension = new Lazy<NotificationServiceExtension>(Create);
+
+        public static NotificationServiceExtension Current
+        {
+            get
+            {
+                return extension.Value;
+            }
+
+        }
+
         public void DidReceiveNotificationRequest(UNNotificationRequest request, Action<UNNotificationContent> contentHandler)
         {
             iOS.KumulosNotificationService.DidReceiveNotificationRequest(request, contentHandler);
-           
+        }
+
+        static NotificationServiceExtension Create()
+        {
+            return new NotificationServiceExtension();
         }
     }
 }
