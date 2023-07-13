7f2b17944e6d67d8a6a5029d850da6f165bdb63e
Andy Lindsay <andy_l@optimove.com>
2023-07-13 10:21:26 +0100

    Revert exception message, bump nuget build version.

--- KumulosSDK.DotNet/Kumulos.cs
+++ KumulosSDK.DotNet/Kumulos.cs
@@ -61,7 +61,7 @@ namespace KumulosSDK.DotNet
 
         internal static Exception NotImplementedInReferenceAssembly()
         {
-            return new NotImplementedException("BOOM.");
+            return new NotImplementedException("This functionality is not implemented in the portable version of this assembly.  You should reference the NuGet package from your main application project in order to reference the platform-specific implementation.");
         }
     }
 }
